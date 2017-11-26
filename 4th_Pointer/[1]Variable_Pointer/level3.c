ba34954aec10678e35787ae13a01fe2734eca598
happynote3966 <01ssrmikururudevice01@gmail.com>
2017-11-26 22:37:03 +0900

    modify directory

---
+++ 4th_Pointer/[1]Variable_Pointer/level3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+	int variable = 100;
+	int *pointer = &variable;
+	printf("variable is %f\n",/*here*/(float)/*here*/*pointer);
+	return 0;
+}
---
+++ 4th_Pointer/[1]Variable_Pointer/level4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+	unsigned int variable = 0x41;
+	printf("variable character is %c\n",(char)variable);
+	return 0;
+}
